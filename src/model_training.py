8da132916d8f974681a431180e173a8716448bb1
Shree-Pastagia <shreepastagia126219@gmail.com>
2026-04-07 18:03:20 +0530

    added cross validation

--- src/model_training.py
+++ src/model_training.py
@@ -1,18 +1,32 @@
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 
 def train_model(X, y):
 
-    # Split dataset
+    # Create model
+    model = RandomForestClassifier()
+
+    # Cross Validation
+    scores = cross_val_score(
+        model,
+        X,
+        y,
+        cv=5
+    )
+
+    print(
+        "Cross Validation Accuracy:",
+        scores.mean()
+    )
+
+    # Train-test split
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
         test_size=0.2,
         random_state=42
     )
-
-    # Create model
-    model = RandomForestClassifier()
 
     # Train model
     model.fit(X_train, y_train)
